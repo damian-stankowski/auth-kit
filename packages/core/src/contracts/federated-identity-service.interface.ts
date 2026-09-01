2732c485ff48b3cb7c699638650719a33ebc9ca8
Damián Rafal Stankowski <damian.stankowski@blinkdating.app>
2026-09-01 23:37:52 +0200

    feat(core): add FederatedIdentityService interface for federated authentication

--- packages/core/src/contracts/federated-identity-service.interface.ts
+++ packages/core/src/contracts/federated-identity-service.interface.ts
@@ -1,5 +1,5 @@
 import type { FederatedCredentials } from "../models/federated-credentials.class";
 
-export interface FederatedAuthenticationService<TProvider extends string = string, TCredentials = unknown> {
+export interface FederatedIdentityService<TProvider extends string = string, TCredentials = unknown> {
   authenticateAsync(): Promise<FederatedCredentials<TProvider, TCredentials>>;
 }
